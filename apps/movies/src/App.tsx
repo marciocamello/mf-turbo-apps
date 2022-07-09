d12f6daceccbebf1d124ff7c4a9b151ccb1b0a81
marciocamello <marciocamello@oulook.com>
2022-07-09 11:14:20 +0100

    First commit

---
+++ apps/movies/src/App.tsx
@@ -0,0 +1,32 @@
+import path from 'path';
+import React from 'react';
+
+import { AppShell } from 'ui';
+
+function App() {
+    return <AppShell
+        title="Movies"
+        routes={[
+            {
+                path: "/",
+                element: () => <div>Home</div>,
+            },
+            {
+                path: "/playlist",
+                element: () => <div>Playlist</div>,
+            }
+        ]}
+        navLinks={[
+            {
+                label: "Home",
+                path: "/",
+            },
+            {
+                label: "Playlist",
+                path: "/playlist",
+            }
+        ]}
+    />;
+}
+
+export default App;
